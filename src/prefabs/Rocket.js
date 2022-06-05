de3ab1a2ca27183aaf21e38ec31d078e19ccec38
bputaski <91684470+bputaski@users.noreply.github.com>
2022-06-05 12:52:28 -0700

    adjusted rocket speed

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,7 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
         
         scene.add.existing(this);
         this.firing = false;
-        this.moveSpeed = 2;
+        this.moveSpeed = 3;
 
         this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
