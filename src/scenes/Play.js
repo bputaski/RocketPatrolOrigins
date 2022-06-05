f72c6b17c9118dac18c990dc6fbb98c981d17279
bputaski <91684470+bputaski@users.noreply.github.com>
2022-06-05 02:45:44 -0700

    added in spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene{
     preload(){
         this.load.image('starfield','assets/starfield.png')
         this.load.image('rocket','assets/rocket.png');
+        this.load.image('ship', 'assets/ship.png');
     }
 
     create(){
@@ -27,6 +28,12 @@ class Play extends Phaser.Scene{
    
         this.p1Rocket = new Rocket(this, 0, 0, 'rocket').setOrigin(0.5, 0);
         this.p1Rocket.reset();
+        
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'ship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'ship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'ship', 0, 10).setOrigin(0,0);
+
 
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -36,10 +43,14 @@ class Play extends Phaser.Scene{
 
     update(){
         this.p1Rocket.update();
+        
+        // update spaceships
+        this.ship01.update();               
+        this.ship02.update();
+        this.ship03.update();
 
         this.starfield.tilePositionX -=4;       
         
-
         const movementSpeed = 3;
         if (keyLEFT.isDown){
             this.p1Rocket.x -= movementSpeed;
